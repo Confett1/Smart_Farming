a56515855ea1ed272525a0f1fe17ab19595e124f
cabatinganvictorio <cabatinganvictorio@gmail.com>
2025-02-25 21:11:27 +0800

    ud

--- my-app/src/utils/toast.jsx
+++ my-app/src/utils/toast.jsx
@@ -7,21 +7,58 @@ export const toast = (text, title, icon) =>
     text: text ? text : "",
     title: title ? title : "",
     icon,
-    confirmButtonColor: "#134991",
+    confirmButtonColor: "#22c55e",
   })
 
-export const toastConfirm = (text, title, icon) =>
-  swal.fire({
-    title,
-    text,
-    icon,
-    confirmButtonColor: "#134991",
-    showCancelButton: true,
-    confirmButtonText: "Yes",
-    // showClass: {
-    //   popup: "animate__animated animate__fadeInDown",
-    // },
-    // hideClass: {
-    //   popup: "animate__animated animate__fadeOutUp",
-    // },
-  })
+  export const toastConfirm = (text, title, icon) =>
+    swal.fire({
+      title,
+      text,
+      icon,
+      showCancelButton: true,
+      confirmButtonColor: "#22c55e",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swal.fire({
+          title: "Deleted!",
+          text,
+          icon: "success"
+        });
+      }
+    });
+
+   export const signedIn = () => {
+    const Toast = swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = swal.stopTimer;
+        toast.onmouseleave = swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "success",
+      title: "Signed in successfully"
+    });
+   }
+
+// export const toastConfirm = (text, title, icon) =>
+//   swal.fire({
+//     title,
+//     text,
+//     icon,
+//     confirmButtonColor: "#134991",
+//     showCancelButton: true,
+//     confirmButtonText: "Yes",
+//     // showClass: {
+//     //   popup: "animate__animated animate__fadeInDown",
+//     // },
+//     // hideClass: {
+//     //   popup: "animate__animated animate__fadeOutUp",
+//     // },
+//   })
