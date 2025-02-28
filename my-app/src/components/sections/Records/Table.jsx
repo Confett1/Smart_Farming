6a60ec4feb02a9d7b58c812b3340609708d16d55
Agustin Pelicano Jr. <jayarpelicano01@gmail.com>
2025-02-28 23:58:40 +0800

    Records Data

--- my-app/src/components/sections/Records/Table.jsx
+++ my-app/src/components/sections/Records/Table.jsx
@@ -50,7 +50,7 @@
 
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
@@ -72,26 +72,29 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import PropTypes from 'prop-types';  // Importing PropTypes
 import { FormControlLabel, Switch } from '@mui/material';
+import API from '../../../api/api';
 
-function createData(id, name, calories, fat, carbs, protein) {
-  return { id, name, calories, fat, carbs, protein };
-}
+// function createData(id, name, calories, fat, carbs) {
+//   return { id, name, calories, fat, carbs };
+// }
 
-const rows = [
-  createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-  createData(2, 'Donut', 452, 25.0, 51, 4.9),
-  createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-  createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-  createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
-  createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-  createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-  createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-  createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-  createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-  createData(13, 'Oreo', 437, 18.0, 63, 4.0),
-];
+
+
+// const rows = [
+//   createData(1, 'Cupcake', 305, 3.7, 67),
+//   createData(2, 'Donut', 452, 25.0, 51 ),
+//   createData(3, 'Eclair', 262, 16.0, 24),
+//   createData(4, 'Frozen yoghurt', 159, 6.0, 24),
+//   createData(5, 'Gingerbread', 356, 16.0, 49),
+//   createData(6, 'Honeycomb', 408, 3.2, 87),
+//   createData(7, 'Ice cream sandwich', 237, 9.0, 37),
+//   createData(8, 'Jelly Bean', 375, 0.0, 94),
+//   createData(9, 'KitKat', 518, 26.0, 65),
+//   createData(10, 'Lollipop', 392, 0.2, 98),
+//   createData(11, 'Marshmallow', 318, 0, 81),
+//   createData(12, 'Nougat', 360, 19.0, 9),
+//   createData(13, 'Oreo', 437, 18.0, 63),
+// ];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -110,11 +113,10 @@ function getComparator(order, orderBy) {
 }
 
 const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: 'activityName', numeric: false, disablePadding: true, label: 'Activity Name' },
+  { id: 'timestamp', numeric: true, disablePadding: false, label: 'Date' },
+  { id: 'duration', numeric: true, disablePadding: false, label: 'Duration' },
+  { id: 'status', numeric: true, disablePadding: false, label: 'Status' },
 ];
 
 function EnhancedTableHead(props) {
@@ -202,12 +204,29 @@ EnhancedTableToolbar.propTypes = {
 };
 
 export default function RecordTable() {
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [order, setOrder] = React.useState('desc');
+  const [orderBy, setOrderBy] = React.useState('timestamp');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = React.useState([]);
+
+  useEffect(() => {
+    const fetchRecordData = async () => {
+      try {
+        const response = await API.get('/records');
+        setRows(response.data);
+      } catch (error) {
+        console.error("Error fetching Records", error);
+        
+      }
+    }
+    fetchRecordData();
+
+    const interval = setInterval(fetchRecordData, 10000);
+    return () => clearInterval(interval);
+  }, [])
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -260,7 +279,7 @@ export default function RecordTable() {
       [...rows]
         .sort(getComparator(order, orderBy))
         .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
-    [order, orderBy, page, rowsPerPage]
+    [order, orderBy, page, rowsPerPage, rows]
   );
 
   return (
@@ -288,12 +307,11 @@ export default function RecordTable() {
                       <Checkbox color="primary" checked={isItemSelected} inputProps={{ 'aria-labelledby': labelId }} />
                     </TableCell>
                     <TableCell component="th" id={labelId} scope="row" padding="none">
-                      {row.name}
+                      {row.activityName}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell align="right">{new Date(row.timestamp).toLocaleDateString()}</TableCell>
+                    <TableCell align="right">{row.duration} {row.duration === 1 ? "Minute" : "Minutes"}</TableCell>
+                    <TableCell align="right">{row.status}</TableCell>
                   </TableRow>
                 );
               })}
