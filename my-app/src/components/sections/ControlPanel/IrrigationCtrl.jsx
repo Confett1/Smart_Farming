66b2bda7937a5721cc1c3217b51bfdaf120f9b8c
cabatinganvictorio <cabatinganvictorio@gmail.com>
2025-03-06 16:50:09 +0800

    revamp

--- my-app/src/components/sections/ControlPanel/IrrigationCtrl.jsx
+++ my-app/src/components/sections/ControlPanel/IrrigationCtrl.jsx
@@ -8,21 +8,9 @@ const IrrigationControl = () => {
                     <span className="status active">Active</span>
                 </div>
                 <div className="control-body">
-                    <div className="control-item">
-                        <label>Water Flow Rate</label>
-                        <input type="range" min="0" max="100" value="50" className="slider" id="waterFlow" />
-                        <span className="value">50%</span>
-                    </div>
-                    <div className="control-item">
-                        <label>Schedule</label>
-                        <select id="irrigationSchedule">
-                            <option value="1">Every 1 hour</option>
-                            <option value="2">Every 2 hours</option>
-                            <option value="4">Every 4 hours</option>
-                        </select>
-                    </div>
+              
                     <button className="control-btn" id="startIrrigation">
-                        <i className="fas fa-play"></i> Start Irrigation
+                        <i className="fas fa-power-off"></i> 
                     </button>
                 </div>
             </div>
