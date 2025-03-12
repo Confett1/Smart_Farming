59c693cef2c440e911f03eab2ba09d01cd5865cd
cabatinganvictorio <cabatinganvictorio@gmail.com>
2025-03-12 08:43:53 +0800

    charts

--- my-app/src/components/sections/Charts/index.jsx
+++ my-app/src/components/sections/Charts/index.jsx
@@ -8,12 +8,11 @@ const ChartsComponent = () => {
     return (
         <>
             <header>
-                <div className={`page-name ${darkModePref ? "text-[#2c3e50]" : "text-gray-200"}`}>                    
+                <div className={`page-name flex items-center justify-between ${darkModePref ? "text-[#2c3e50]" : "text-gray-200"}`}>                    
                     <h2>Charts Statistics</h2>
-                </div>
-                <div className="ml=[750px] flex-2 gap-4 controls">
+                <div className="controls">
                     <select 
-                        className="w-full p-2 border border-gray-300 rounded bg-white"
+                        className="w-full p-2 border border-gray-300 rounded bg-white cursor-pointer text-sm text-gray-700"
                         id="period-select"
                         value={selectedPeriod}
                         onChange={(e) => setSelectedPeriod(e.target.value)}
@@ -23,9 +22,10 @@ const ChartsComponent = () => {
                         <option value="lastMonth">Last Month</option>
                         <option value="lastYear">Last Year</option>
                     </select>
-                    <button className="hover:bg-[var(--primary-color)] hover:text-white w-full bg-green-400 border text-[#080710] py-1.8 mx-3 text-lg rounded cursor-pointer " id="export-btn">
+                    <button className="hover:bg-[var(--primary-color)] hover:text-white w-full bg-green-400 border-0 text-gray-6\700 py-1.8 mx-3 text-xs rounded cursor-pointer duration-300" id="export-btn">
                         Export Data
                     </button>
+                </div>
                 </div>
             </header>
 
