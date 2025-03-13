55df1ac5e938dfdc64b4b226d4b80060c3753731
Agustin Pelicano Jr. <jayarpelicano01@gmail.com>
2025-03-14 00:23:57 +0800

    change password

--- my-app/src/components/sections/Settings/Security.jsx
+++ my-app/src/components/sections/Settings/Security.jsx
@@ -1,10 +1,72 @@
 // import { Divider } from "@mui/material";
 
-const SecuritySettings = (formData, handleChange) => {
+import { useState } from "react";
+import API from "../../../api/api";
+import { toast } from "../../../utils/toast";
+
+
+const SecuritySettings = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const [passwords, setPasswords] = useState({
+    id: user.userId,
+    currentPassword: '',
+    newPassword: '',
+    confirmNewPassword: '',
+  });
+
+  console.log(passwords);
+  
+
+  
+  const handleChange = (e) => {
+    setPasswords({
+      ...passwords,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleChangePass = async (e) => {
+    e.preventDefault();
+
+    if (!passwords.newPassword ||  !passwords.confirmNewPassword || !passwords.currentPassword) {
+      toast("Please fill out all the fields", "","warning");
+      return;
+    }
+
+    if (passwords.newPassword != passwords.confirmNewPassword) {
+      toast("New Password doesn't match", "","warning");
+      return;
+    }
+
+    try {
+      const response = await API.post('/user/changePasswords', passwords);
+      const toastResponse = await toast(response?.data, "Password Update", "success");
+      
+      if (toastResponse.isConfirmed) {
+        setPasswords({
+          currentPassword: '',
+          newPassword: '',
+          confirmNewPassword: '',
+        });
+      }
+    } catch (err) {
+      const toastResponse = await toast(err.response.data , "Password change error", "error");
+      if (toastResponse.isConfirmed) {
+        setPasswords({
+          currentPassword: '',
+          newPassword: '',
+          confirmNewPassword: '',
+        });
+      }
+    }
+  }
+
   const darkModePref = JSON.parse(localStorage.getItem('darkmode'));
   return (
+    
+    <form onSubmit={handleChangePass}>
     <div className={`${darkModePref ? "bg-white text-gray-600" : "text-gray-200 bg-gray-800"} p-4 rounded-xl shadow duration-300`}>
-      <div className={`border-b border-gray-100`}>
+      <div className={ `border-b border-gray-100`}>
         <h3 className="text-lg font-semibold">Security and Privacy</h3>
         <p className="text-sm pb-2.5">Manage your security settings</p>
       </div>
@@ -17,9 +79,10 @@ const SecuritySettings = (formData, handleChange) => {
           style={{backgroundColor: "white"}}
           type="password"
           placeholder="Current password"
-          name="currentpassword"
+          name="currentPassword"
+          value={passwords.currentPassword}
           onChange={handleChange}
-          id="currentpassword" 
+          id="currentPassword" 
           className={`mt-1 block w-86 py-3 px-2 text-[#6c746c] text-sm font-normal rounded-lg border border-gray-300 focus:outline-[#d8eed5de] focus:outline-offset-2 focus-within:border-green-700 focus:duration-50`}/>
 
         </div>
@@ -29,11 +92,12 @@ const SecuritySettings = (formData, handleChange) => {
           </div>
           <input 
           style={{backgroundColor: "white"}}
+          value={passwords.newPassword}
           type="password"
           placeholder="New password"
-          name="newpassword"
+          name="newPassword"
           onChange={handleChange}
-          id="newpassword" 
+          id="newPassword" 
           className="mt-1 block w-86 py-3 px-2 text-[#6c746c] text-sm font-normal rounded-lg border border-gray-300 focus:outline-[#d8eed5de] focus:outline-offset-2 focus-within:border-green-700 focus:duration-50"/>
 
         </div>
@@ -44,16 +108,19 @@ const SecuritySettings = (formData, handleChange) => {
           <input 
           style={{backgroundColor: "white"}}
           type="password"
+          value={passwords.confirmNewPassword}
           placeholder="Confirm password"
-          name="confirmpassword"
+          name="confirmNewPassword"
           onChange={handleChange}
           id="confirmpassword" 
           className="mt-1 block w-86 py-3 px-2 text-[#6c746c] text-sm font-normal rounded-lg border border-gray-300 focus:outline-[#d8eed5de] focus:outline-offset-2 focus-within:border-green-700 focus:duration-50"/>
-
+      
         </div>
-        {/* <Divider sx={{ my: 1.3 }} /> */}
-      </div>
+        <div className="flex justify-end">
+          <button type="submit" className="bg-gray-700 text-white px-6 w-86 py-3 rounded-lg hover:bg-gray-500 my-3">SUBMIT</button></div>
+        </div>
     </div>
+    </form>
   );
 };
 
